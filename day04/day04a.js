a1d1eac1a01cd7f4c088541e474e50e329c9e646
schmidko <einfach.schmidt@gmail.com>
2022-12-16 08:19:13 +0100

    day4

---
+++ day04/day04a.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const array = fs.readFileSync('data04big.txt').toString().split("\n");
+
+let sum = 0;
+for (const [key, line] of array.entries()) {
+    const ranges = line.split(',');
+    const left = getNumbers(ranges[0]);
+    const right = getNumbers(ranges[1]);
+
+    const result1 = isOverlapping(left, right);
+    const result2 = isOverlapping(right, left);
+    if (result1 || result2) {
+        sum++;
+    }
+}
+
+console.log(sum);
+
+function isOverlapping(left, right) {
+    let fullOverlapping = true;
+    for (const x of right) {
+        if (!left.includes(x)) {
+            fullOverlapping = false;
+            break;
+        }
+    }
+    if (fullOverlapping) {
+        return true;
+    }
+    return false;
+}
+
+function getNumbers(range) {
+    const startEnd = range.split('-');
+    const start = parseInt(startEnd[0]);
+    const end = parseInt(startEnd[1]);
+    const numbers = [];
+    for (let i=start; i<=end; i++) {
+        numbers.push(parseInt(i));
+    }
+    return numbers;
+}
