8ea64a9441b742264e7263971529272cc161c02b
schmidko <einfach.schmidt@gmail.com>
2022-12-25 12:01:58 +0100

    Added day 11

---
+++ day11/day11a.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const arrayLines = fs.readFileSync('data11big.txt').toString().split("\n");
+
+let monkeys = [];
+for (const [index, line] of arrayLines.entries()) {
+    if (line[0] === 'M') {
+        const items = arrayLines[index + 1].replace('  Starting items: ', '').split(' ').map((e) => parseInt(e));
+        const operation = arrayLines[index + 2].replace("  Operation: new = old ", '').split(' ');
+        operation[1] = parseInt(operation[1]);
+        const test = parseInt(arrayLines[index + 3].replace("  Test: divisible by ", ''));
+        const ifTrue = parseInt(arrayLines[index + 4].replace("   If true: throw to monkey ", '').trim());
+        const ifFalse = parseInt(arrayLines[index + 5].replace("   If false: throw to monkey ", '').trim());
+        monkeys.push({items, operation, test, ifTrue, ifFalse, count: 0});
+    }
+
+}
+
+for (let i = 0; i < 20; i++) {
+    for (const monkey of monkeys) {
+        for (let item of monkey.items) {
+            monkey.count++;
+            itemLvl = calculate(item, monkey.operation[0], monkey.operation[1]);
+            itemLvl = Math.floor(itemLvl / 3);
+            if (itemLvl % monkey.test === 0) {
+                monkeys[monkey.ifTrue].items.push(itemLvl);
+            } else {
+                monkeys[monkey.ifFalse].items.push(itemLvl);
+            }
+        }
+        monkey.items = [];
+    }
+}
+
+function calculate(a, o, b) {
+    if (isNaN(b)) {
+        b = a;
+    }
+    if (o === '+') {
+        return a + b;
+    }
+    if (o === '*') {
+        return a * b;
+    }
+}
+
+monkeys.sort((a,b) => b.count-a.count);
+console.log(monkeys[0].count * monkeys[1].count);
